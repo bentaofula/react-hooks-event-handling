7e818189074b4638d99c7b444e0a9c8af273fbd4
Benta <bentaakitwi500@gmail.com>
2023-04-21 11:40:59 +0300

    added file

---
+++ src/components/Keypad.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Keypad(){
+    function handleChange(){
+        console.log("Entering password...")
+    }
+    return (  
+<form>
+    <input type="password" onChange={handleChange}/>
+</form>
+    );
+}
+export default Keypad;
